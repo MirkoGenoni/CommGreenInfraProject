ef386e7b14f02f5d6d54829431bdd95d0d025acb
ratatoskr - Mirko <ratatoskr@mail.massaman-labs.de>
2025-12-11 15:44:37 +0100

    Add leaf area eaten as function to diff eq

--- transmitter_model.m
+++ transmitter_model.m
@@ -69,29 +69,50 @@ fclose(sanizited_data);
 data_emission = readtable("Data/Transmitter/sanitized_emission_data.csv", ...
     'PreserveVariableNames',true);
 
-% INTERPOLATION
+% EMISSION INTERPOLATION
 t=0:5;
 emission=data_emission.larvae_2;
 
-dt= 1;
+dt= 0.001;
 t_em_intrp=0:dt:5;
 
 emission_intrp = interp1(t, emission,t_em_intrp,'linear');
+
+% EXTRACT STRESSOR FROM CURRENT EMISSION
+leaf_cons = zeros(1,length(t_em_intrp));
+for index= 1:length(t_em_intrp)
+    temp = find(round(stressor,4)==round(emission_intrp(index),4));
+    leaf_cons(1,index) = t_stressor(round((max(temp) + min(temp))/2)-1);
+end
+
+%PLOT STRESSOR PROFILE
+figure;
+plot(t_em_intrp, leaf_cons);
+xlabel("Days");
+ylabel("Leaf area eaten [cm^2]", 'Interpreter', 'tex');
+fontsize(16,"points");
 
 figure;
 plot(t_em_intrp,emission_intrp);
 hold on;
 scatter(t,emission);
 
+% LEAST SQUARE ON DIFFERENTIAL EQUATION
 x0=[1,1,1];
 [param_fit,resnorm,residual,eflag,output] = lsqnonlin( ...
     @(params) ODE_fit(params(1),params(2),params(3), ...
-    t_stressor,stressor,t_em_intrp,emission_intrp), ...
+    t_em_intrp,leaf_cons,t_em_intrp,emission_intrp), ...
     x0);
 
+% SOLVE DIFFERENTIAL EQUATION WITH OPTIMAL PARAMETERS
 tsolv=[0 5];
 ic = emission_intrp(1);
 [t,sol] = ode45(@(t,g) ODE_eq(t,g,param_fit(1),param_fit(2), ...
-    param_fit(3), t_stressor, stressor),tsolv,ic);
+    param_fit(3), t_em_intrp, leaf_cons),tsolv,ic);
 sol_intrp = interp1(t,sol,t_em_intrp);
 plot(t_em_intrp,sol_intrp);
+xlabel("Days");
+ylabel("LOX emission [nmol m^{-2} s^{-1}]", 'Interpreter', 'tex');
+fontsize(16,"points");
+legend('Emission interpolation', 'Experimental points', ...
+    'Emission fitted to differential eq.');
