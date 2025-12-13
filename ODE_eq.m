dca0e5244ae26297c64c6dceb16274ffd0ca8f29
ratatoskr - Mirko <ratatoskr@mail.massaman-labs.de>
2025-12-13 16:07:36 +0100

    Add error plot to all fitting and different initial conditions for LS applied to differential equation

--- ODE_eq.m
+++ ODE_eq.m
@@ -1,4 +1,4 @@
-function dgdt = ODE_eq(t,g,w,c,k_d,st,s)
-    s = interp1(st,s,t);
-    dgdt = (max(g)./ (1+exp(-w*s + c)))-k_d*g;
+function dgdt = ODE_eq(t,g,w,c,k_d,p)
+    s = polyval(p,t);
+    dgdt = max(g)./(1+exp(-w*s + c))-k_d*g;
 end
--- ODE_solve.m
+++ ODE_solve.m
@@ -1,7 +1,7 @@
-function out = ODE_fit(w,c,k_d,t_stressor,stressor, t_plot,expected_emission)
+function out = ODE_solve(w,c,k_d, p, t_plot,expected_emission)
     tsolv=[0 5];
     ic = expected_emission(1);
-    [t,sol] = ode45(@(t,g) ODE_eq(t,g,w,c,k_d,t_stressor, stressor), ...
+    [t,sol] = ode45(@(t,g) ODE_eq(t,g,w,c,k_d,p), ...
         tsolv,ic);
 
     solution_intrp = interp1(t,sol,t_plot);
