59d291075906c1a64674934e4b70f8aa9ba97916
Sufian Kuriry <sufian.k@outlook.com>
2026-01-10 01:22:50 +0100

    Updating receiver model V3

--- receiver_model_V3.m
+++ receiver_model_V3.m
@@ -2,21 +2,16 @@
 clear; clc; close all;
 
 %% Channel parameters (from 'multiple_puff_ch_model_t.m')
-CairData = load('Cair.mat');
-c_air = CairData.C_air_vector;
-c_air = c_air/max(c_air);
-t = CairData.t_arr;
+c_air_2sec = load('Data/Channel/concentration_delta_t_0.001s_2_seconds_x_1_m.mat');
+c_air_5days = load('Data/Channel/concentration_delta_t_1000s_5_days_x_01_m.mat');
+c_air_5days_1m = load('Data/Channel/concentration_delta_t_1000s_5_days_x_1_m.mat');
+c_air = c_air_5days_1m.C_air_vector_1;
+t_2Sec = load('Data/Channel/time_delta_t_0.001s_2_seconds_x_1_m.mat');
+t_5Day = load('Data/Channel/time_delta_t_1000s_5_days.mat');
+%t = t_2Sec.t_arr;
+t = t_5Day.t_arr;
 dt = mean(diff(t));
 Nt   = length(t);
-
-% Simulating longer emmission rate (for saturation test)
-% extend_time = 500;
-% t = dt:dt:(extend_time+Nt);
-% Nt = length(t);
-% extention = ones(1,Nt-(500));
-% c_air = [c_air extention];
-% c_air = c_air * 5;
-
 
 % Plot for verification
 figure
@@ -38,7 +33,7 @@ K_LA    = 500;               % liquid-side mass transfer coeff [m/s]
 %K_L    = 1;                 % For experimentation;
 g_s_mol    = 0.85;           % stomatal conductance [m/s]
 g_s = g_s_mol * R * T / P;   % convertion to m/s
-k_m    = 1/200;              % metabolic rate [1/s]
+k_m    = 0;              % metabolic rate [1/s]
 
 %% Derived coefficients (FULL EXPRESSIONS)
 alpha = (A_leaf * g_s) / (K_LA * V_L);   % [1/s]
@@ -61,6 +56,17 @@ for n = 2:Nt
     kernel = exp(-gamma*(t(n) - t(1:n)));
     C_L_SOL(n) = beta * trapz(t(1:n), kernel .* c_air(1:n));
 end
+
+%% Saturation Condition and Time to saturation
+% Saturation
+C_L_SS = (beta/gamma) * max(c_air);
+fprintf("For constant air concentration A0 = %f \n", max(c_air));
+fprintf("The saturation concentration in the leaf CL_SS = %f \n", C_L_SS);
+% Time to certain leaf concentration
+C_L_targ =  C_L_SS*0.5;
+t_sat = -(1/gamma)*log(1-(C_L_targ/C_L_SS));
+fprintf("Time to reach 50%% of internal concentration: \n");
+fprintf("%d hours \n", t_sat/(60*60));
 
 %% Ploting the results
 figure; hold on; grid on;
